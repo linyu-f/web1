b39c56fb994b1810eebb94bd55457bc0a4d938e2
linyu-f <1274282051@qq.com>
2020-11-04 10:01:11 +0800

    123

---
+++ 第十二次课/js初步学习.js
@@ -0,0 +1,8 @@
+function orange(){
+    var array = [1,2,3,45,56,234,23,87,68]
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        console.log(element);
+    }
+}
+orange();
