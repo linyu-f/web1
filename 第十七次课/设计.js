d10e5110cc06b09e31aa9372fcb1d6a6511049e3
linyu-f <1274282051@qq.com>
2020-12-02 09:59:50 +0800

    123

--- 第十七次课/设计.js
+++ 第十七次课/设计.js
@@ -9,5 +9,7 @@ Object2.prototype2 = {
 var obj1 = new Object2()
 console.log(obj1.__proto2__.__proto2__);
 
-
-
+function pear2(name){
+    return "this is "+`${name}!`
+}
+console.log(pear2("桃子"));
